ac658f73fb845d9d952bf0718138a0f3c520adaa
dragdev <samueljohnnicholson@gmail.com>
2020-03-01 19:30:18 +0000

    __

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const toplevel = document.getElementById("toplevel")
 if (toplevel === undefined || toplevel === null) {
   var elem = document.createElement("DIV")
+  elem.style = "width: 99%;padding: 10px;height: 25%;font-family: monospace"
   elem.innerHTML = `<span class="text-warning">Unable to locate element "toplevel". Some functions may not work properly</span><br><span class="highlight-info">Expected Type: div<br>Got: {elem}</span>`
 }
 
