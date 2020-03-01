ec9568f5e3c3751726fbc119a46826987907570d
dragdev <samueljohnnicholson@gmail.com>
2020-03-01 19:26:31 +0000

    Testing new feature

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const toplevel = document.getElementById("toplevel")
+if (toplevel === undefined || toplevel === null) {
+  var elem = document.createElement("DIV")
+  elem.innerHTML = `<span class="text-warning">Unable to locate element "toplevel". Some functions may not work properly</span><br><span class="highlight-info">Expected Type: div<br>Got: {elem}</span>`
+}
+
 function r(url) {
   if (url === undefined) {
     var url = "index.html"
   }
   window.location.href = url
+}
+
+function fetch(url) {}
+  if (url === undefined) {
+    var url = "https://ptb.discordapp.com/api/guilds/504504527093891074/widget.json"
+  }
+
 }
