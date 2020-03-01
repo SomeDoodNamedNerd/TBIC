4be593444fd43b19d3913874927763aa907757e0
dragdev <samueljohnnicholson@gmail.com>
2020-03-01 19:31:05 +0000

    Fixed my dodgey JS and added debugging stuff

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const toplevel = document.getElementById("toplevel")
 if (toplevel === undefined || toplevel === null) {
+  console.log(`Getting toplevel returned ${toplevel}`)
   var elem = document.createElement("DIV")
   elem.style = "width: 99%;padding: 10px;height: 25%;font-family: monospace"
-  elem.innerHTML = `<span class="text-warning">Unable to locate element "toplevel". Some functions may not work properly</span><br><span class="highlight-info">Expected Type: div<br>Got: {elem}</span>`
+  elem.innerHTML = `<span class="text-warning">Unable to locate element "toplevel". Some functions may not work properly</span><br><span class="highlight-info">Expected Type: div<br>Got: ${elem}</span>`
 }
 
 function r(url) {
