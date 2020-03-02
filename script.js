2a415169423b32bee83fc6f43cb3675231dd48b2
dragdev <samueljohnnicholson@gmail.com>
2020-03-02 22:45:14 +0000

    Fixed some things in some files for some lines for some codes n stuff k

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const toplevel = document.getElementById("toplevel")
+alert()
 console.log(toplevel)
 if (toplevel === undefined || toplevel === null) {
   console.log(`Getting toplevel returned ${toplevel}`)
-  var elem = document.createElement("DIV")
-  elem.style = "width: 99%;padding: 10px;height: 25%;font-family: monospace"
-  elem.innerHTML = `<span class="text-warning">Unable to locate element "toplevel". Some functions may not work properly</span><br><span class="highlight-info">Expected Type: div<br>Got: ${elem}</span>`
+  document.write(`<div style='width:99%;height:99%;background-color: white;padding:8px'><span class="text-warning">Unable to locate element "toplevel". Some functions may not work properly</span><br><span class="highlight-info">Expected Type: div<br>Got: ${toplevel}</span></div>`)
 }
 
 function r(url) {
@@ -14,7 +13,7 @@ function r(url) {
   window.location.href = url
 }
 
-function fetch(url) {}
+function fetch(url) {
   if (url === undefined) {
     var url = "https://ptb.discordapp.com/api/guilds/504504527093891074/widget.json"
   }
